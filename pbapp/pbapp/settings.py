0c93c6084f3c97ffdef56b2d6a345d0b9cfa0a72
Hub972 <hassouvie@gmail.com>
2019-07-19 15:51:51 +0200

    Change settings

--- pbapp/pbapp/settings.py
+++ pbapp/pbapp/settings.py
@@ -128,17 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
+STATIC_URL = '/static/'
 if os.environ.get('ENV') == 'PRODUCTION':
-
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_DIR, 'static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
