f4cf291f619b4b3847c4a2fb005267bdc5f036d6
Hub972 <hassouvie@gmail.com>
2019-07-19 15:32:51 +0200

    Change settings

--- pbapp/pbapp/settings.py
+++ pbapp/pbapp/settings.py
@@ -133,8 +133,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
