d55e5cce76b2396c937f45dc5e1409cbf8ad8f6a
Hub <hassouvie@gmail.com>
2019-08-04 22:35:10 +0200

    add winlab branch

--- pbapp/pbapp/urls.py
+++ pbapp/pbapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from store import views
@@ -25,4 +27,4 @@ urlpatterns = [
     url('store/', include('store.urls', namespace='store')),
     url('', views.index)
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
